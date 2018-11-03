24a0d8b0a199d0dd16b372d212c43e8e0e72f11a
André Martins <andre7@engcomp.grad.ufg.br>
2018-11-03 18:20:27 -0300

    Add files via upload

---
+++ Marmitaria/src/Utilitarios/FundoTela.java
@@ -0,0 +1,25 @@
+
+package Utilitarios;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
+
+
+public class FundoTela extends JDesktopPane{
+
+    private static final long serialVersionUID = 1L;
+    private Image imagem;
+    
+public FundoTela(String imagem) {
+    this.imagem = new ImageIcon (imagem).getImage();
+    
+}
+@Override
+public void paintComponent(Graphics g){
+    g.drawImage(imagem, 0, 0, getWidth(), getHeight(), this);
+    
+}
+
+}
